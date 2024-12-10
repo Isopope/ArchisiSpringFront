f30749433e95b8af31f0d3eb8ad9d3f151854ee8
isopope <nkoubah@gmail.com>
2024-12-10 18:53:36 +0100

    integration de leaflet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { TerrainListComponent } from './terrain-list/terrain-list.component';
 import { TerrainFormComponent } from './terrain-form/terrain-form.component';
 import { RouterModule } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatSlideToggleModule, TerrainListComponent,TerrainFormComponent,RouterModule ],
+  imports: [MatSlideToggleModule, TerrainListComponent,TerrainFormComponent,RouterModule, MapComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
